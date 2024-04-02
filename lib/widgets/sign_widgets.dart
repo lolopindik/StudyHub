c34d92d6b8d56800055a051c881efbf505de64e9
lolopindik <dimaluka2014@gmail.com>
2024-04-02 18:44:35 +0300

    removed aytoleading option from appbar

--- lib/widgets/sign_widgets.dart
+++ lib/widgets/sign_widgets.dart
@@ -3,6 +3,8 @@ import 'package:study_hub/preferences/app_theme.dart';
 
   PreferredSizeWidget? buildSignAppBar(BuildContext context) {
     return AppBar(
+      //отключение автоматического arrow back
+      automaticallyImplyLeading: false,
       backgroundColor: AppTheme.mainColor,
       title: const Text(
         'StudyHub',
