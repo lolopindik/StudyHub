83e0ca25a96a8db6717fbdbd0151f8d9fab6d156
lolopindik <dimaluka2014@gmail.com>
2024-04-03 05:09:11 +0300

    working on auth

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,60 +1,116 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:study_hub/preferences/app_theme.dart';
 
 class SignUpPage extends StatelessWidget {
-   SignUpPage({super.key});
+  SignUpPage({super.key});
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 120),
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: Container(
-          height: 340,
-          width: 319,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: AppTheme.secondaryColor),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  'Регистрация',
-                  style: TextStyles.ruberoidLight32,
-                ),
-              ),
-              Container(
-                height: 264,
-                width: 319,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: AppTheme.mainColor,
-                ),
-                child:  Column(
-                  children: <Widget>
-                [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Container(
-                      width: 282,
-                      height: 51,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(60),
-                        color: AppTheme.signElementColor
-                        ),
-                    ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 120),
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            height: 340,
+            width: 319,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: AppTheme.secondaryColor),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    'Регистрация',
+                    style: TextStyles.ruberoidLight32,
                   ),
-                ],
                 ),
-              )
-            ],
+                Container(
+                  height: 264,
+                  width: 319,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: AppTheme.mainColor,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Container(
+                          width: 282,
+                          height: 51,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(60),
+                            color: AppTheme.signElementColor,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20, top: 23),
+                            child: TextField(
+                              controller: emailController,
+                              style: TextStyles.ruberoidLight16,
+                              decoration: const InputDecoration(
+                                hintText: 'Email:',
+                                hintStyle: TextStyles.ruberoidLight20,
+                                border: InputBorder.none,
+                              ),
+                              cursorColor: Colors.white70,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Container(
+                          width: 282,
+                          height: 51,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(60),
+                            color: AppTheme.signElementColor,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20, top: 23),
+                            child: TextField(
+                              controller: passwordController,
+                              style: TextStyles.ruberoidLight16,
+                              decoration: const InputDecoration(
+                                hintText: 'Password:',
+                                hintStyle: TextStyles.ruberoidLight20,
+                                border: InputBorder.none,
+                              ),
+                              cursorColor: Colors.white70,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 61),
+                        child: InkWell(
+                          onTap: () {
+                            //TODO_Добавить переход на на этап заполнения данных 
+                          },
+                          child: Container(
+                            width: 282,
+                            height: 51,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              color: AppTheme.secondaryColor,
+                            ),
+                            child: const Center(
+                                child: Text('Подтвердить',
+                                    style: TextStyles.ruberoidLight20)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/preferences/app_theme.dart
+++ lib/preferences/app_theme.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static const Color mainColor = Color.fromARGB(255, 43, 40, 40); //2B2828
   static const Color secondaryColor = Color.fromARGB(255, 27, 27, 27); //1B1B1B
-  static const Color signElementColor = Color.fromARGB(83, 27, 27, 27);
+  static const Color signElementColor = Color.fromARGB(83, 27, 27, 27); //! цвет не совпадает с макетом
 }
 
 class TextStyles {
+  static const TextStyle ruberoidLight16 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 16, color: Colors.white, );
+  static const TextStyle ruberoidLight20 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 20, color: Colors.white, );
   static const TextStyle ruberoidLight28 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 28, color: Colors.white, );
   static const TextStyle ruberoidLight32 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 32, color: Colors.white, );
   static const TextStyle ruberoidRegular40 = TextStyle(fontFamily: 'Ruberoid-Regular', fontSize: 40, color: Colors.white, );
