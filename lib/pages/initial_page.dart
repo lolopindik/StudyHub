3bcd1dd8de5b75058eaf095fd41c21e8871047c6
lolopindik <dimaluka2014@gmail.com>
2024-04-05 01:33:43 +0300

    rolling back code due to logic problems

--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // Импорт Firebase Authentication
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:study_hub/pages/sign_up_In.dart';
 import 'package:study_hub/pages/welcome_page.dart';
 import 'package:study_hub/pages/home_page.dart';
@@ -10,8 +10,8 @@ class InitialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<User?>(
-      future: _getCurrentUser(),
+    return FutureBuilder<bool>(
+      future: _isFirstTime(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Scaffold(
@@ -25,23 +25,28 @@ class InitialPage extends StatelessWidget {
         }
 
         // Проверяем, аутентифицирован ли пользователь
-        if (snapshot.hasData && snapshot.data != null) {
+        if (snapshot.hasData && snapshot.data == false) {
           return const HomePage(); // Если пользователь аутентифицирован, переходим на HomePage
         }
 
-        // Проверяем, если это новый пользователь, направляем на WelcomePage
-        if (snapshot.connectionState == ConnectionState.done && !snapshot.hasData) {
+        // Пользователь не аутентифицирован, продолжаем с WelcomePage или SignUpInPage
+        if (snapshot.hasData && snapshot.data == true) {
+          _setFirstTime(false); 
           return const WelcomePage();
         }
-
-        // Пользователь не аутентифицирован, продолжаем с SignUpInPage
         return const SignUpInPage();
       },
     );
   }
 
-  Future<User?> _getCurrentUser() async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    return auth.currentUser;
+  Future<bool> _isFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? isLoggedIn = prefs.getBool('isLoggedIn');
+    return isLoggedIn ?? true; // Если значение null, считаем, что пользователь не аутентифицирован
+  }
+
+  void _setFirstTime(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isLoggedIn', value);
   }
 }
