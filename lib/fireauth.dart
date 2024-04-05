ef4a718ecf1d891c8503c8a2285c6895b700e6c8
lolopindik <dimaluka2014@gmail.com>
2024-04-05 05:47:13 +0300

    Changed the project launch logic for better implementation of transition between pages, as well as optimization and bug fixes

---
+++ lib/fireauth.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FirebaseService {
+  static final FirebaseService _singleton = FirebaseService._internal();
+  factory FirebaseService() => _singleton;
+  FirebaseService._internal();
+
+  final auth = FirebaseAuth.instance;
+  final currentUser = FirebaseAuth.instance.currentUser;
+
+  Future<bool> isFirstLaunch() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('first_time') ?? true;
+  }
+
+  void setFirstLaunch(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('first_time', value);
+  }
+
+  onListenUser(void Function(User?)? doListen)
+  {
+    FirebaseAuth.instance.authStateChanges().listen(doListen);
+  }
+
+  onLogin() async {}
+
+  onRegister() async {}
+
+  logOut() async {}
+
+  verifyemail() async {}
+
+  getCurrentUser() {}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,44 @@
+// ignore_for_file: library_private_types_in_public_api, avoid_print
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:study_hub/pages/initial_page.dart';
+import 'package:study_hub/fireauth.dart';
+import 'package:study_hub/pages/home_page.dart';
+import 'package:study_hub/pages/sign_up_in.dart';
+import 'package:study_hub/pages/welcome_page.dart';
+import 'firebase_options.dart';
 
-  void main() async {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
   await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-}
 
-class App extends StatelessWidget {
-  const App({super.key});
+  final FirebaseService firebaseService = FirebaseService();
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  firebaseService.onListenUser((user) async {
+    bool isFirstLaunch = await firebaseService.isFirstLaunch();
+
+    Widget initialPage;
+
+    if (user == null) {
+      if (isFirstLaunch) {
+        initialPage = const WelcomePage();
+        print('no user');
+      } else {
+        initialPage = const SignUpInPage();
+        print('no user');
+      }
+    } else {
+      initialPage = const HomePage();
+      print('user $user');
+    }
+
+    runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: InitialPage(),
-    );
-  }
+      home: initialPage,
+    ));
+  });
 }
