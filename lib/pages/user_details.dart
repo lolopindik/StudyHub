6fec4fe510eb324a631b1776c93627f9b1a85559
lolopindik <dimaluka2014@gmail.com>
2024-04-09 11:23:20 +0300

    user details are divided into functionality and visuals

--- lib/pages/user_details.dart
+++ lib/pages/user_details.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:study_hub/pages/home_page.dart';
-import 'package:study_hub/preferences/app_theme.dart';
 import 'package:study_hub/widgets/sign_appbar.dart';
+import 'package:study_hub/widgets/user_form.dart';
 
 class UserData extends StatefulWidget {
   const UserData({super.key});
@@ -16,134 +16,14 @@ class _UserDataState extends State<UserData> {
   final tokenController = TextEditingController();
 
   @override
-  Widget build(context) {
-    FirebaseDatabase.instance.ref().child('UserDetails');
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildSignAppBar(context),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('lib/assets/images/background.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 120),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  height: 340,
-                  width: 319,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    color: AppTheme.secondaryColor,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          'Данные',
-                          style: TextStyles.ruberoidLight32,
-                        ),
-                      ),
-                      Container(
-                        height: 264,
-                        width: 319,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40),
-                          color: AppTheme.mainColor,
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(top: 20),
-                              child: Container(
-                                width: 282,
-                                height: 51,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(60),
-                                  color: AppTheme.signElementColor,
-                                ),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.only(left: 20, top: 5),
-                                  child: TextField(
-                                    controller: fullnameController,
-                                    style: TextStyles.ruberoidLight16,
-                                    decoration: const InputDecoration(
-                                      hintText: 'Full name:',
-                                      hintStyle: TextStyles.ruberoidLight20,
-                                      border: InputBorder.none,
-                                    ),
-                                    cursorColor: Colors.white70,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Container(
-                                width: 282,
-                                height: 51,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(60),
-                                  color: AppTheme.signElementColor,
-                                ),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.only(left: 20, top: 5),
-                                  child: TextField(
-                                    obscureText: true,
-                                    controller: tokenController,
-                                    style: TextStyles.ruberoidLight16,
-                                    decoration: const InputDecoration(
-                                      hintText: 'Course token:',
-                                      hintStyle: TextStyles.ruberoidLight20,
-                                      border: InputBorder.none,
-                                    ),
-                                    cursorColor: Colors.white70,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 61),
-                              child: InkWell(
-                                onTap: () {
-                                  sendDataToFirebase();
-                                },
-                                child: Container(
-                                  width: 282,
-                                  height: 51,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(60),
-                                    color: AppTheme.secondaryColor,
-                                  ),
-                                  child: const Center(
-                                    child: Text(
-                                      'Подтвердить',
-                                      style: TextStyles.ruberoidLight20,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
+      body: UserDataForm(
+        fullnameController: fullnameController,
+        tokenController: tokenController,
+        sendDataToFirebase: sendDataToFirebase,
+        navigateToHomepage: navigateToHomepage,
       ),
     );
   }
@@ -191,3 +71,4 @@ class _UserDataState extends State<UserData> {
     );
   }
 }
+
---
+++ lib/widgets/user_form.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:study_hub/preferences/app_theme.dart';
+
+class UserDataForm extends StatelessWidget {
+  final TextEditingController fullnameController;
+  final TextEditingController tokenController;
+  final Function sendDataToFirebase;
+  final Function navigateToHomepage;
+
+  const UserDataForm({
+    super.key, 
+    required this.fullnameController,
+    required this.tokenController,
+    required this.sendDataToFirebase,
+    required this.navigateToHomepage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('lib/assets/images/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 120),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 340,
+                width: 319,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: AppTheme.secondaryColor,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        'Данные',
+                        style: TextStyles.ruberoidLight32,
+                      ),
+                    ),
+                    Container(
+                      height: 264,
+                      width: 319,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: AppTheme.mainColor,
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Container(
+                              width: 282,
+                              height: 51,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(60),
+                                color: AppTheme.signElementColor,
+                              ),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 20, top: 5),
+                                child: TextField(
+                                  controller: fullnameController,
+                                  style: TextStyles.ruberoidLight16,
+                                  decoration: const InputDecoration(
+                                    hintText: 'Full name:',
+                                    hintStyle: TextStyles.ruberoidLight20,
+                                    border: InputBorder.none,
+                                  ),
+                                  cursorColor: Colors.white70,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Container(
+                              width: 282,
+                              height: 51,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(60),
+                                color: AppTheme.signElementColor,
+                              ),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 20, top: 5),
+                                child: TextField(
+                                  obscureText: true,
+                                  controller: tokenController,
+                                  style: TextStyles.ruberoidLight16,
+                                  decoration: const InputDecoration(
+                                    hintText: 'Course token:',
+                                    hintStyle: TextStyles.ruberoidLight20,
+                                    border: InputBorder.none,
+                                  ),
+                                  cursorColor: Colors.white70,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 61),
+                            child: InkWell(
+                              onTap: () {
+                                sendDataToFirebase();
+                              },
+                              child: Container(
+                                width: 282,
+                                height: 51,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(60),
+                                  color: AppTheme.secondaryColor,
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    'Подтвердить',
+                                    style: TextStyles.ruberoidLight20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
