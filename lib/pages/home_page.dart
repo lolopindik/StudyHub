9e4880bf013a84ae9ada5066d1d66744c4488eaa
lolopindik <dimaluka2014@gmail.com>
2024-04-10 12:15:15 +0300

    made some template changes based on what I found on stackoverflow

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:study_hub/preferences/app_theme.dart';
 import 'package:study_hub/widgets/home_appbar.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,15 +11,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  //TODO: расположить подвязку к бд с чтением и записью
-
+  late DatabaseReference userRef;
   String? _userId;
+  Map<String, dynamic>? _userData;
+  Map<String, dynamic>? _courseData;
 
   @override
   void initState() {
     super.initState();
-    _getCurrentUser(); 
+    _getCurrentUser();
   }
 
   void _getCurrentUser() async {
@@ -27,8 +27,35 @@ class _HomePageState extends State<HomePage> {
     if (user != null) {
       setState(() {
         _userId = user.uid;
+        userRef = FirebaseDatabase.instance.ref().child("UserDetails").child(_userId!);
       });
+      DataSnapshot snapshot = await userRef.get();
+      setState(() {
+        _userData = snapshot.value as Map<String, dynamic>?; 
+      });
+      _fetchCourseDetails();
     }
+  }
+
+  void _fetchCourseDetails() async {
+    if (_userData != null && _userData!['courseProgress'] != null) {
+      String courseToken = _userData!['courseProgress']['courseToken'];
+      DatabaseReference courseRef = FirebaseDatabase.instance.ref().child("Courses").child(courseToken);
+      DataSnapshot snapshot = await courseRef.get();
+      setState(() {
+        _courseData = snapshot.value as Map<String, dynamic>?; 
+      });
+      // Here you can proceed with any further actions with user and course data
+      // For example, you can track the user's progress in the course
+      _trackUserProgress();
+    }
+  }
+
+  void _trackUserProgress() {
+    // Implement your logic for tracking user progress here
+    // For example, you can check which lessons the user has completed
+    // based on the data retrieved from _userData and _courseData
+    // Update UI or perform any other actions accordingly
   }
 
   @override
@@ -36,13 +63,13 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: buildHomeAppBar(context),
       body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('lib/assets/images/background.png'),
-              fit: BoxFit.cover,
-            ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/images/background.png'),
+            fit: BoxFit.cover,
           ),
         ),
+      ),
     );
   }
 }
