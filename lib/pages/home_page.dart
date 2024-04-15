d9a0a6d600de809622e7ebfe45752d0eb5d7f056
lolopindik <dimaluka2014@gmail.com>
2024-04-15 11:14:08 +0300

    added ListView and func compare tokes rendered

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: use_build_context_synchronously, avoid_print
-
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:study_hub/pages/user_details.dart';
+import 'package:study_hub/preferences/app_theme.dart';
 import 'package:study_hub/widgets/home_appbar.dart';
+import 'package:study_hub/fire%D0%B0func.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,7 +18,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _getCurrentUser();
-    compareTokens();
   }
 
   void _getCurrentUser() async {
@@ -32,124 +29,116 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<List<Map<String, dynamic>>> compareTokens() async {
-    int counter = 10; //! ограниченное число итераций по базе данных
-    bool tokenFound = false;
-    List<Map<String, dynamic>> coursesData = [];
-
-    DatabaseReference userTokenRef = FirebaseDatabase.instance
-        .ref()
-        .child("UserDetails/$_userId/courseProgress/courseToken");
-    DataSnapshot userTokenSnapshot = await userTokenRef.get();
-    String? userToken = userTokenSnapshot.value as String?;
-
-    String itemCourse = "course_";
-    for (var i = 1; i <= counter; i++) {
-      String currentCourseNumber = itemCourse + i.toString();
-      DatabaseReference courseTokenRef = FirebaseDatabase.instance
-          .ref()
-          .child("Courses/$currentCourseNumber/token");
-      DataSnapshot courseTokenSnapshot = await courseTokenRef.get();
-      String? courseToken = courseTokenSnapshot.value as String?;
-
-      if (courseToken != null && userToken == courseToken) {
-        tokenFound = true;
-        DatabaseReference courseNameRef = FirebaseDatabase.instance
-            .ref()
-            .child("Courses/$currentCourseNumber/courseName");
-        DataSnapshot courseNameSnapshot = await courseNameRef.get();
-        String? courseName = courseNameSnapshot.value as String?;
-
-        if (courseName != null) {
-          Map<String, dynamic> courseData = {
-            'courseName': courseName,
-            'subjects': <Map<String, dynamic>>[]
-          };
-
-          String itemSubjects = "subject_";
-          for (var j = 1; j <= counter; j++) {
-            String currentSubjectNumber = itemSubjects + j.toString();
-            DatabaseReference subjectItemRef = FirebaseDatabase.instance
-                .ref()
-                .child(
-                    "Courses/$currentCourseNumber/subjects/$currentSubjectNumber");
-            DataSnapshot subjectSnapshot = await subjectItemRef.get();
-            Map<dynamic, dynamic>? subjectData =
-                subjectSnapshot.value as Map<dynamic, dynamic>?;
-
-            if (subjectData != null) {
-              Map<String, dynamic> subjectDetails = {
-                'name': subjectData['name'],
-                'lessons': <Map<String, dynamic>>[]
-              };
-
-              // Добавление параметра lessonComplete для каждого урока
-              subjectData['lessons'].forEach((key, value) {
-                Map<String, dynamic> lessonDetails = {
-                  'name': value['name'],
-                  'documents': value['documents'],
-                };
-
-                // Проверка, был ли урок уже отмечен как завершенный
-                if (value['lessonComplete'] == null) {
-                  // Если урок не был отмечен как завершенный, устанавливаем значение по умолчанию (0)
-                  lessonDetails['lessonComplete'] = 0;
-                } else {
-                  // Если урок уже был отмечен как завершенный, оставляем текущее значение
-                  lessonDetails['lessonComplete'] = value['lessonComplete'];
-                }
-
-                (subjectDetails['lessons'] as List<Map<String, dynamic>>)
-                    .add(lessonDetails);
-              });
-
-              (courseData['subjects'] as List<Map<String, dynamic>>)
-                  .add(subjectDetails);
-            }
-          }
-          coursesData.add(courseData);
-        }
-      }
-    }
-
-    // Обновление данных пользователя в базе данных
-    DatabaseReference userCourseRef = FirebaseDatabase.instance
-        .ref()
-        .child("UserDetails/$_userId/courseProgress");
-    await userCourseRef.update({"coursesData": coursesData});
-
-    print(coursesData);
-
-    if (!tokenFound) {
-      // Проверяем, был ли найден токен после всех итераций
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Указан неверный токен!'),
-        ),
-      );
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => const UserData(),
-        ),
-      );
-      return []; // возвращаем пустой список
-    }
-
-    return coursesData; // возвращаем данные о курсах
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildHomeAppBar(context),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/assets/images/background.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/assets/images/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              height: MediaQuery.of(context).size.height * 0.55,
+              decoration: const BoxDecoration(
+                color: AppTheme.secondaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: compareTokens(_userId), // Передаем _userId в функцию
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                        child: CircularProgressIndicator(
+                      color: Colors.white60,
+                    ));
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    final List<Map<String, dynamic>> coursesData =
+                        snapshot.data ?? [];
+                    return ListView.builder(
+                      itemCount: coursesData.length,
+                      itemBuilder: (context, index) {
+                        final Map<String, dynamic> courseData =
+                            coursesData[index];
+                        final List<Map<String, dynamic>> subjects =
+                            courseData['subjects'] ?? [];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 10),
+                            ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: subjects.length,
+                              itemBuilder: (context, index) {
+                                final Map<String, dynamic> subjectDetails =
+                                    subjects[index];
+                                return Padding(
+                                  padding: const EdgeInsets.only(top: 10),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 15),
+                                        child: Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.95,
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.08,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(40),
+                                              color: AppTheme.mainElementColor),
+                                          child: Center(
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.only(left: 15, right: 15),
+                                              child: Text(
+                                                subjectDetails['name'] ??
+                                                    'Subject Name',
+                                                style:
+                                                    TextStyles.ruberoidLight20,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
