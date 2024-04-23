b911cebe38080f0d4a93a974db2c03388af519a7
lolopindik <dimaluka2014@gmail.com>
2024-04-24 01:00:06 +0300

    added functionality and page visuals

--- lib/pages/lesson_details.dart
+++ lib/pages/lesson_details.dart
@@ -1,19 +1,61 @@
-// ignore_for_file: deprecated_member_use
+// ignore_for_file: avoid_print, deprecated_member_use, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:study_hub/preferences/app_theme.dart';
+import 'package:study_hub/widgets/customLoadingIndicator.dart';
 import 'package:study_hub/widgets/lessons_appbar.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LessonDetails extends StatelessWidget {
+class LessonDetails extends StatefulWidget {
   final Map<String, dynamic> lessonData;
-  final inputAnswer = TextEditingController();
 
-  LessonDetails({required this.lessonData, super.key});
+  const LessonDetails({super.key, required this.lessonData});
+
+  @override
+  LessonDetailsState createState() => LessonDetailsState();
+}
+
+class LessonDetailsState extends State<LessonDetails> {
+  final inputAnswer = TextEditingController();
+  String? _userId;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUser().then((_) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    if (widget.lessonData['userAnswer'] != null) {
+      inputAnswer.text = widget.lessonData['userAnswer'];
+    }
+  }
+
+  Future<void> _getCurrentUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        _userId = user.uid;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<dynamic> documents = lessonData['documents']?.values.toList() ?? [];
+    if (_isLoading) {
+      return Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: CustomLoadingIndicator(),
+        ),
+      );
+    }
+
+    List<dynamic> documents = widget.lessonData['documents']?.values.toList() ?? [];
 
     return Scaffold(
       appBar: buildLessonAppBar(context),
@@ -29,7 +71,7 @@ class LessonDetails extends StatelessWidget {
                 child: LayoutBuilder(
                   builder: (context, constraints) {
                     return Text(
-                      lessonData['name'] ?? 'Lesson',
+                      widget.lessonData['name'] ?? 'Lesson',
                       style: constraints.maxWidth > 150
                           ? TextStyles.ruberoidRegular20
                           : TextStyles.ruberoidRegular28,
@@ -132,7 +174,20 @@ class LessonDetails extends StatelessWidget {
                             color: Colors.white54,
                           ),
                           onPressed: () {
-                            debugPrint('tap');
+                            String answer = inputAnswer.text;
+                            if (answer.isEmpty) {
+                              return;
+                            }
+                            int lessonComplete = widget.lessonData['lessonComplete'] ?? 0;
+                            if (lessonComplete == 2) {
+                              _showConfirmationDialog(answer);
+                              return;
+                            }
+                            sendDataToFirebase(
+                              _userId!,
+                              widget.lessonData['name'] ?? '',
+                              answer,
+                            );
                           },
                         ),
                       ),
@@ -144,6 +199,111 @@ class LessonDetails extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Future<void> sendDataToFirebase(String userId, String lessonName, String answer) async {
+    DatabaseReference userCourseRef = FirebaseDatabase.instance
+        .reference()
+        .child("UserDetails/$userId/courseProgress");
+    DataSnapshot userCourseSnapshot = await userCourseRef.get();
+    Map<dynamic, dynamic>? userCourseData =
+        userCourseSnapshot.value as Map<dynamic, dynamic>?;
+    if (userCourseData != null) {
+      List<dynamic> coursesData =
+          (userCourseData['coursesData'] as List<dynamic>).toList();
+
+      bool lessonFound = false;
+
+      for (var courseData in coursesData) {
+        List<dynamic>? subjects = courseData['subjects'];
+        if (subjects != null) {
+          for (var subject in subjects) {
+            List<dynamic>? lessons = subject['lessons'];
+            if (lessons != null) {
+              for (var lesson in lessons) {
+                if (lesson['name'] == lessonName) {
+                  lesson['userAnswer'] = answer;
+                  lesson['lessonComplete'] = 3;
+                  lessonFound = true;
+                  break;
+                }
+              }
+            }
+            if (lessonFound) {
+              break;
+            }
+          }
+        }
+        if (lessonFound) {
+          break;
+        }
+      }
+
+      if (!lessonFound) {
+        Map<String, dynamic> newLesson = {
+          'name': lessonName,
+          'userAnswer': answer,
+          'lessonComplete': 3,
+        };
+        
+        for (var courseData in coursesData) {
+          List<dynamic>? subjects = courseData['subjects'];
+          if (subjects != null && subjects.isNotEmpty) {
+            (subjects.first['lessons'] as List).add(newLesson);
+            break;
+          }
+        }
+      }
+
+      await userCourseRef.update({
+        "coursesData": coursesData,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Ответ успешно отправлен'),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Данные не были отправлены'),
+        ),
+      );
+    }
+  }
+
+  Future<void> _showConfirmationDialog(String answer) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppTheme.mainColor,
+          surfaceTintColor: Colors.transparent,
+          title: const Text("Предупреждение!", style: TextStyles.ruberoidLight20,),
+          content: const Text("Вы уверены, что хотите заменить верный ответ? Для его подтверждения нужна будет повторная проверка" , style: TextStyles.ruberoidLight16,),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Отмена", style: TextStyles.ruberoidLight16,),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                sendDataToFirebase(
+                  _userId!,
+                  widget.lessonData['name'] ?? '',
+                  answer,
+                );
+              },
+              child: const Text("Подтвердить",style: TextStyles.ruberoidLight16,),
+            ),
+          ],
+        );
+      },
     );
   }
 }
