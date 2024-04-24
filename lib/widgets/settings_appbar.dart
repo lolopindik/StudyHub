57500ac5a44d047e4833f0032055728e296b8e33
lolopindik <dimaluka2014@gmail.com>
2024-04-24 16:30:42 +0300

    added new appbar for settings

---
+++ lib/widgets/settings_appbar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:study_hub/preferences/app_theme.dart';
+
+PreferredSizeWidget? buildSettingsAppBar(BuildContext context) {
+  return AppBar(
+    scrolledUnderElevation: 0.0,
+    automaticallyImplyLeading: false,
+    title: const Text(
+      'Настройки',
+      style: TextStyles.ruberoidRegular28,
+    ),
+    centerTitle: true,
+    backgroundColor: AppTheme.mainColor,
+    leading: Padding(
+      padding: const EdgeInsets.only(left: 15),
+      child: Container(
+        width: 38,
+        height: 38,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: AppTheme.secondaryColor,
+        ),
+        child: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 23,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    ),
+  );
+}
