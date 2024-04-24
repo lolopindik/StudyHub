80031d772d326f7e8c9d8932e8fef595493657ad
lolopindik <dimaluka2014@gmail.com>
2024-04-24 16:31:53 +0300

    added rotation on settings

--- lib/widgets/home_appbar.dart
+++ lib/widgets/home_appbar.dart
@@ -1,39 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:study_hub/pages/settings.dart';
 import 'package:study_hub/preferences/app_theme.dart';
 
-  PreferredSizeWidget? buildHomeAppBar(BuildContext context) {
-    return AppBar(
-      //отключение автоматического arrow back
-      scrolledUnderElevation: 0.0,
-      automaticallyImplyLeading: false,
-      backgroundColor: AppTheme.mainColor,
-      title: const Text(
-        'StudyHub',
-        style: TextStyles.ruberoidRegular28,
-      ),
-      centerTitle: true,
-      actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 15),
-          child: Container(
-            width: 38,
-            height: 38,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: AppTheme.secondaryColor,
-            ),
-            child: IconButton(
-              onPressed: () {
-                // переход к page-настройки 
-              },
-              icon: const Icon(
-                Icons.tune,
-                size: 23,
-                color: Colors.grey,
-              ),
+PreferredSizeWidget? buildHomeAppBar(BuildContext context) {
+  return AppBar(
+    //отключение автоматического arrow back
+    scrolledUnderElevation: 0.0,
+    automaticallyImplyLeading: false,
+    backgroundColor: AppTheme.mainColor,
+    title: const Text(
+      'StudyHub',
+      style: TextStyles.ruberoidRegular28,
+    ),
+    centerTitle: true,
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 15),
+        child: Container(
+          width: 38,
+          height: 38,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppTheme.secondaryColor,
+          ),
+          child: IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => UserSettings(context)),
+              );
+            },
+            icon: const Icon(
+              Icons.tune,
+              size: 23,
+              color: Colors.grey,
             ),
           ),
-        )
-      ],
-    );
-  }
+        ),
+      )
+    ],
+  );
+}
