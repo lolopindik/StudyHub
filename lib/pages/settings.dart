c303040f0bf1d4bf35ee558381fed9284d14f10e
lolopindik <dimaluka2014@gmail.com>
2024-04-26 01:14:31 +0300

    layout from the layout :)

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -36,29 +36,62 @@ class UserSettings extends StatelessWidget {
                     const SizedBox(height: 20,),
                     Container(
                       height: MediaQuery.of(context).size.height * 0.08,
+                      width: MediaQuery.of(context).size.height * 0.5,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(60),
                         color: AppTheme.mainElementColor
                       ),
                       constraints:const BoxConstraints(maxWidth: 450, minHeight: 40),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Icon(Icons.person, size: 30, color: Colors.white54,),
+                            SizedBox(width: 20,),
+                            Text('Аккаунт', style: TextStyles.ruberoidLight20,)
+                          ],
+                        ),
+                      ),
                     ),
                     const SizedBox(height: 20,),
                     Container(
                       height: MediaQuery.of(context).size.height * 0.08,
+                      width: MediaQuery.of(context).size.height * 0.5,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(60),
                         color: AppTheme.mainElementColor
                       ),
                       constraints:const BoxConstraints(maxWidth: 450, minHeight: 40),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Icon(Icons.logout, size: 30, color: Colors.white54,),
+                            SizedBox(width: 20,),
+                            Text('Выход', style: TextStyles.ruberoidLight20,)
+                          ],
+                        ),
+                      ),
                     ),
                     const SizedBox(height: 20,),
                     Container(
                       height: MediaQuery.of(context).size.height * 0.08,
+                      width: MediaQuery.of(context).size.height * 0.5,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(60),
                         color: AppTheme.mainElementColor
                       ),
                       constraints:const BoxConstraints(maxWidth: 450, minHeight: 40),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Icon(Icons.delete_forever_outlined, size: 30, color: Colors.white54,),
+                            SizedBox(width: 20,),
+                            Text('Удаление Аккаунта', style: TextStyles.ruberoidLight20,)
+                          ],
+                        ),
+                      ),
                     ),
                     const SizedBox(height: 20,),
                     const Text(
@@ -75,29 +108,42 @@ class UserSettings extends StatelessWidget {
                     const SizedBox(height: 20,),
                     Container(
                       height: MediaQuery.of(context).size.height * 0.08,
+                      width: MediaQuery.of(context).size.height * 0.5,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(60),
                         color: AppTheme.mainElementColor
                       ),
                       constraints:const BoxConstraints(maxWidth: 450, minHeight: 40),
-                    ),
-                    const SizedBox(height: 20,),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.08,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(60),
-                        color: AppTheme.mainElementColor
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Icon(Icons.code, size: 30, color: Colors.white54,),
+                            SizedBox(width: 20,),
+                            Text('О программе', style: TextStyles.ruberoidLight20,)
+                          ],
+                        ),
                       ),
-                      constraints: const BoxConstraints(maxWidth: 450, minHeight: 40),
                     ),
                     const SizedBox(height: 20,),
                     Container(
                       height: MediaQuery.of(context).size.height * 0.08,
+                      width: MediaQuery.of(context).size.height * 0.5,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(60),
                         color: AppTheme.mainElementColor
                       ),
                       constraints:const BoxConstraints(maxWidth: 450, minHeight: 40),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Icon(Icons.bug_report_outlined, size: 30, color: Colors.white54,),
+                            SizedBox(width: 20,),
+                            Text('Сообщить об ошибке', style: TextStyles.ruberoidLight20,)
+                          ],
+                        ),
+                      ),
                     ),
                   ],
                 ),
