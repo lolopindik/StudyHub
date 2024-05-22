0bfde3292c3ca8a0e939337041af51794e264a06
lolopindik <dimaluka2014@gmail.com>
2024-05-22 11:37:59 +0300

    added new fonts

--- lib/preferences/app_theme.dart
+++ lib/preferences/app_theme.dart
@@ -11,7 +11,9 @@ class AppTheme {
 }
 
 class TextStyles {
+  static const TextStyle ruberoidLight15 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 15, color: Colors.white, );
   static const TextStyle ruberoidLight16 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 16, color: Colors.white, );
+  static const TextStyle ruberoidLight18 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 18, color: Colors.white, );
   static const TextStyle ruberoidLight20 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 20, color: Colors.white, );
   static const TextStyle ruberoidLight28 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 28, color: Colors.white, );
   static const TextStyle ruberoidLight32 = TextStyle(fontFamily: 'Ruberoid-Light', fontSize: 32, color: Colors.white, );
