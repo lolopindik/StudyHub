6af81c8f49a7a8a805b393e0b54e91d3a28d4ac8
lolopindik <dimaluka2014@gmail.com>
2024-05-28 00:44:26 +0300

    The beta course visual is ready, all that remains is to add functionality and make edits if bugs are found

--- lib/pages/lesson_details.dart
+++ lib/pages/lesson_details.dart
@@ -26,119 +26,103 @@ class _LessonDetailsState extends State<LessonDetails> {
       appBar: buildLessonAppBar(context),
       backgroundColor: AppTheme.secondaryColor,
       body: RawScrollbar(
-        thumbColor: Colors.white70,
-        thickness: 2,
+        thumbColor: AppTheme.mainColor,
+        thickness: 4,
         radius: const Radius.circular(10.0),
         child: SingleChildScrollView(
           child: Column(
             children: [
               Container(
+                width: double.infinity,
                 height: MediaQuery.of(context).size.height * 0.103,
                 color: AppTheme.mainElementColor,
                 constraints: const BoxConstraints(minHeight: 60),
                 child: Center(
                   child: Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15),
-                      child: Text(widget.lessonData['name'] ?? 'Lesson',
-                          style: TextStyles.ruberoidRegular20)),
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Text(widget.lessonData['name'] ?? 'Lesson',
+                        style: TextStyles.ruberoidRegular20),
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (theory.isNotEmpty) ...[
-                      Text(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (theory.isNotEmpty)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, left: 15, right: 10),
+                      child: Text(
                         theory,
                         style: TextStyles.ruberoidLight20,
                       ),
-                      const SizedBox(height: 40),
-                    ],
-                    if (answers != null && question.isNotEmpty) ...[
-                      Center(
-                        child: Container(
-                          constraints: const BoxConstraints(maxWidth: 450),
-                          decoration: const BoxDecoration(
-                            color: AppTheme.mainColor,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(40),
-                              topRight: Radius.circular(40),
-                            ),
-                          ),
+                    ),
+                  const SizedBox(height: 40),
+                  if (answers != null && question.isNotEmpty)
+                    Column(
+                      children: [
+                        Center(
                           child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 10),
-                            child: LayoutBuilder(
-                              builder: (context, constraints) {
-                                return Center(
-                                  child: Text(
-                                    question,
-                                    style: TextStyles.ruberoidRegular20,
-                                    textAlign: TextAlign.center,
-                                    overflow: TextOverflow
-                                        .clip,
-                                  ),
-                                );
-                              },
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Container(
+                              constraints: const BoxConstraints(maxWidth: 450),
+                              decoration: const BoxDecoration(
+                                color: AppTheme.mainColor,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(40),
+                                  topRight: Radius.circular(40),
+                                ),
+                              ),
+                              padding: const EdgeInsets.all(15),
+                              child: Center(
+                                child: Text(
+                                  question,
+                                  style: TextStyles.ruberoidRegular20,
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.clip,
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Center(
-                        child: Container(
-                          height: 10,
-                          color: AppTheme.mainElementColor,
-                          constraints: const BoxConstraints(maxWidth: 450),
-                        ),
-                      ),
-                      Center(
-                        child: Container(
-                          constraints: const BoxConstraints(maxWidth: 450),
-                          child: ListView.builder(
-                            shrinkWrap:true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: answers.length,
-                            itemBuilder: (context, index) {
-                              bool isLastIndex = index == answers.length - 1;
-                              return LayoutBuilder(
-                                builder: (context, constraints) {
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Container(
+                              constraints: const BoxConstraints(maxWidth: 450),
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: answers.length,
+                                itemBuilder: (context, index) {
+                                  bool isLastIndex = index == answers.length - 1;
                                   return Column(
                                     children: [
                                       Container(
-                                        constraints:
-                                            const BoxConstraints(minHeight: 35),
+                                        constraints: const BoxConstraints(minHeight: 60),
                                         decoration: BoxDecoration(
                                           color: AppTheme.mainElementColor,
                                           borderRadius: isLastIndex
                                               ? const BorderRadius.only(
-                                                  bottomLeft:
-                                                      Radius.circular(20),
-                                                  bottomRight:
-                                                      Radius.circular(20),
+                                                  bottomLeft: Radius.circular(20),
+                                                  bottomRight: Radius.circular(20),
                                                 )
                                               : BorderRadius.zero,
                                         ),
                                         child: Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 10, vertical: 10),
+                                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
                                           child: Container(
-                                            height: MediaQuery.of(context)
-                                                    .size
-                                                    .height *
-                                                0.08,
+                                            height: MediaQuery.of(context).size.height * 0.08,
                                             decoration: BoxDecoration(
                                               color: AppTheme.mainColor,
-                                              borderRadius:
-                                                  BorderRadius.circular(60),
+                                              borderRadius: BorderRadius.circular(60),
                                             ),
                                             child: Center(
                                               child: Text(
-                                                  answers[index].toString(),
-                                                  style: TextStyles
-                                                      .ruberoidLight18,
-                                                  textAlign: TextAlign.center,
-                                                  overflow: TextOverflow.clip),
+                                                answers[index].toString(),
+                                                style: TextStyles.ruberoidLight18,
+                                                textAlign: TextAlign.center,
+                                                overflow: TextOverflow.clip,
+                                              ),
                                             ),
                                           ),
                                         ),
@@ -146,33 +130,115 @@ class _LessonDetailsState extends State<LessonDetails> {
                                     ],
                                   );
                                 },
-                              );
-                            },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  const SizedBox(height: 40),
+                  if (entryField)
+                    GestureDetector(
+                      onTap: () {
+                        showModalBottomSheet<void>(
+                          backgroundColor: AppTheme.mainColor,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return buildBottomSheet(context);
+                          },
+                        );
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        constraints: const BoxConstraints(minHeight: 80),
+                        decoration: const BoxDecoration(
+                          color: AppTheme.mainColor,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40),
+                          ),
+                        ),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                const Text(
+                                  'Введите ответ: ',
+                                  style: TextStyles.ruberoidRegular20,
+                                ),
+                                Container(
+                                  width: 38,
+                                  height: 38,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: AppTheme.secondaryColor,
+                                  ),
+                                  child: const Icon(
+                                    Icons.send,
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ],
-                    //todo: реализоать красивый TextField
-                    const SizedBox(height: 40),
-                    if (entryField) ...[
-                      TextField(
-                        controller: inputAnswer,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter your answer',
-                          labelStyle: TextStyles.ruberoidLight16,
-                          hintStyle: TextStyles.ruberoidLight16,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                    ],
-                  ],
-                ),
+                    ),
+                ],
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildBottomSheet(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 15),
+            child: TextField(
+              controller: inputAnswer,
+              cursorHeight: 18,
+              decoration: const InputDecoration(
+                hintText: 'Введите ответ: ',
+                hintStyle: TextStyles.ruberoidLight20,
+                enabledBorder: InputBorder.none,
+              ),
+              style: TextStyles.ruberoidLight16,
+              cursorColor: Colors.white,
+              textAlignVertical: TextAlignVertical.top,
+              expands: true,
+              maxLines: null,
+            ),
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20, bottom: 10),
+              child: Container(
+                width: 38,
+                height: 38,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppTheme.secondaryColor,
+                ),
+                child: const Icon(
+                  Icons.send,
+                  color: Colors.white54,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
