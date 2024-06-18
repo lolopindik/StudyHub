8e94ac48cde3e25209f17e23addaaaf7f518a8fe
lolopindik <dimaluka2014@gmail.com>
2024-06-18 13:53:08 +0300

    i used a viewInsets for reg keyboard

--- lib/pages/lesson_details.dart
+++ lib/pages/lesson_details.dart
@@ -154,6 +154,7 @@ class _LessonDetailsState extends State<LessonDetails> {
                         showModalBottomSheet<void>(
                           backgroundColor: AppTheme.mainColor,
                           context: context,
+                          isScrollControlled: true,
                           builder: (BuildContext context) {
                             return buildBottomSheet(context);
                           },
@@ -208,56 +209,74 @@ class _LessonDetailsState extends State<LessonDetails> {
   }
 
   Widget buildBottomSheet(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 20, left: 15),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: TextField(
-                autofocus: true,
-                controller: inputAnswer,
-                cursorHeight: 18,
-                decoration: const InputDecoration(
-                  hintText: 'Введите ответ: ', //todo добавить изменение текста в state
-                  hintStyle: TextStyles.ruberoidLight20,
-                  enabledBorder: InputBorder.none,
-                  border: InputBorder.none
-                ),
-                style: TextStyles.ruberoidLight16,
-                cursorColor: Colors.white,
-                maxLines: 4,
-              ),
-            ),
-          ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top:25,right: 20, bottom: 10, left: 20),
-              child: Container(
-                width: 38,
-                height: 38,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: AppTheme.secondaryColor,
-                ),
-                child: IconButton(
-                  onPressed: () {
-                    debugPrint(inputAnswer.text);
-                  },
-                  icon: const Icon(
-                    Icons.send,
-                    color: Colors.white54,
+    return Padding(
+      padding: MediaQuery.of(context).viewInsets,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20,bottom: 10),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: MediaQuery.of(context).size.height * 0.3,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 5),
+                        child: TextField(
+                          autofocus: true,
+                          controller: inputAnswer,
+                          cursorHeight: 18,
+                          decoration: const InputDecoration(
+                            hintText: 'Введите ответ: ',
+                            hintStyle: TextStyles.ruberoidLight20,
+                            enabledBorder: InputBorder.none,
+                            border: InputBorder.none,
+                          ),
+                          style: TextStyles.ruberoidLight16,
+                          cursorColor: Colors.white,
+                          maxLines: null,
+                          minLines: 1,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: 20, bottom: 10, left: 20),
+                    child: Container(
+                      width: 38,
+                      height: 38,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppTheme.secondaryColor,
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          debugPrint(inputAnswer.text);
+                        },
+                        icon: const Icon(
+                          Icons.send,
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ],
+      ),
     );
   }
 }
