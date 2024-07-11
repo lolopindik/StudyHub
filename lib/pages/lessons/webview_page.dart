db17f2574868570ebf1ec89018bc95ecbacadc94
lolopindik <dimaluka2014@gmail.com>
2024-07-11 15:50:34 +0300

    webview on test-mode was deployed

---
+++ lib/pages/lessons/webview_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:study_hub/widgets/appbars/webview_appbar.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebviewPage extends StatefulWidget {
+  final String link;
+  const WebviewPage({super.key, required this.link});
+
+  @override
+  State<WebviewPage> createState() => _WebviewPageState();
+}
+
+class _WebviewPageState extends State<WebviewPage> {
+  late String url;
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    url = widget.link;
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(url)
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildWebViewAppBar(context),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
